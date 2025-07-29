a494e571da9e0eb65e3a73190877493f1d56515e
WE-loding <152575029+WE-loding@users.noreply.github.com>
2025-07-29 13:55:43 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,23 +2,82 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+import os,re
+import sys
+import shutil
+
+import jmcomic  
+from PIL import Image
+import astrbot.api.message_components as Comp
+
+
+#PDF导出路径
+PDF_out = 'F:\jmcomic_files'
+
+
+
+
+@register("jmcomic_down", "WE-loding", "下载禁漫本子", "1.0.1")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+    @filter.command("jm")
+    async def get_file(self,event):
+        chain = [
+            Comp.At(qq=event.get_sender_id()),  # At 消息发送者
+            Comp.Plain(" 别急,我先找找,你急我也急（大约还需要等待30s）"),
+        ]
+        
+        yield event.chain_result(chain)
+        
+        
+        coimc_id = event.message_str        # 获取需要的漫画ID      
+        img_folder = 'F:/jmcomic_files/{coimc_id}' # 下载输出路径
+        
+        message = event.message_str
+        s=''.join(re.findall(r'\d+', message))
+        current_file = os.path.abspath(__file__)
+        current_dir = os.path.dirname(current_file)
+        target_file = os.path.join(current_dir, 'option.yml')
+        option = jmcomic.JmOption.from_file(target_file)
+        jmcomic.download_album(s,option) #按照option规定的方式下载指定ID的漫画
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        # 下载完成的使用所有图片，并按文件名升序排序
+        img_files = [f for f in os.listdir(img_folder) if f.lower().endswith(('.jpg'))]
+        img_files.sort()
+        
+        img_list = []
+        
+        # 依次打开所有图片，转换为RGB模式，加入列表
+        for file in img_files:
+            img_path = os.path.join(img_folder, file)
+            img = Image.open(img_path).convert('RGB')
+            img_list.append(img)
+        
+        # 设置导出为固定路径    
+        pdf_path = os.path.join(PDF_out, f"{coimc_id}.pdf")
+        
+        # 设置第一页为封面，其余图片作为内容
+        first_img = img_list[0]
+        rest_imgs = []
+        for img in img_list[1:]:
+            rest_imgs.append(img)
+        
+        # 保存为PDF文件
+        first_img.save(pdf_path, save_all=True, append_images=rest_imgs)
 
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        # 删除图片文件夹
+        shutil.rmtree(img_folder)
+
+        file = [Comp.File(file=f"file://F:/JM_lib/download/{coimc_id}.pdf",name=f"{coimc_id}.pdf")]  # 从本地文件目录发送pdf
+        
+        chain = [
+            Comp.At(qq=event.get_sender_id()),  # At 消息发送者
+            Comp.Plain(" 虽然很艰难,但我还是导出来了"),
+        ]
+        shutil.rmtree('F:/jmcomic_files/{coimc_id}.pdf') # 清理垃圾
+        
+        yield event.chain_result(file)
+
+
+
